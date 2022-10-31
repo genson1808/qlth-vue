e82c2364a5f8c3a5fb11d17117446e6c4288d393
Sơn Trần <genson@Sons-MacBook-Pro.local>
2022-10-31 17:54:44 +0700

    feat: implement router and some component then setup

---
+++ src/components/FooterTable.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="footer-content">
+    <div class="paging-control">
+      <div class="btn-paging btn-first-page pointer"></div>
+      <div class="btn-paging btn-prev-page pointer"></div>
+      <input type="text" class="page-number" value="1" />
+      <div class="btn-paging btn-next-page pointer"></div>
+      <div class="btn-paging btn-last-page pointer"></div>
+      <div class="paging-info">
+        <span> 1/2 trang </span>
+        <span> (56 giáo viên) </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.content {
+  .footer-content {
+    margin: 0 16px 0 16px;
+    height: 60px;
+    box-shadow: -1px -1px 11px 5px rgba(224, 224, 224, 0.92);
+    -webkit-box-shadow: -1px -1px 11px 5px rgba(224, 224, 224, 0.92);
+    -moz-box-shadow: -1px -1px 11px 5px rgba(224, 224, 224, 0.92);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
+.paging-control {
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+}
+.btn-paging {
+  width: 20px;
+  height: 20px;
+  background-color: greenyellow;
+  margin-right: 20px;
+}
+.btn-first-page {
+  background: url(@/assets/Icons/ic_MoveToFirst.png) no-repeat center;
+  background-size: 20px 20px;
+}
+.btn-prev-page {
+  background: url(@/assets/Icons/ic_Back.png) no-repeat center;
+  background-size: 20px 20px;
+}
+.page-number {
+  margin-right: 20px;
+  width: 64px;
+  height: 32px;
+  text-align: center;
+  border: 1px solid var(--line-gridpanel-color);
+  border-radius: 4px;
+  &:focus {
+    outline: none;
+  }
+}
+.btn-next-page {
+  background: url(@/assets/Icons/ic_Next.png) no-repeat center;
+  background-size: 20px 20px;
+}
+.btn-last-page {
+  background: url(@/assets/Icons/ic_MoveToLast.png) no-repeat center;
+  background-size: 20px 20px;
+}
+.paging-info {
+  span {
+    color: #4d4f5c;
+    &:last-child {
+      margin-left: 10px;
+    }
+  }
+}
+</style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="top-toolbar">
+    <div class="toolbar-left">
+      <div class="search">
+        <input type="text" class="input-search" />
+        <div class="search-icon pointer"></div>
+      </div>
+    </div>
+    <div class="toolbar-right dflex">
+      <div class="btn btn--green btn-add">Thêm</div>
+      <div class="btn btn--white btn-export">Xuất khẩu</div>
+      <div class="more-action pointer btn--white"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.top-toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  width: 100%;
+  height: 64px;
+  .toolbar-left {
+    .search {
+      position: relative;
+      min-width: 300px;
+      height: 32px;
+      .input-search {
+        border: 1px solid var(--line-gridpanel-color);
+        border-radius: 4px;
+        width: 100%;
+        height: 100%;
+        padding: 12px 40px 12px 12px;
+        &:focus {
+          outline: none;
+          border: 1px solid var(--main-color);
+        }
+      }
+      .search-icon {
+        position: absolute;
+        background: url(@/assets/Icons/Ic_seerch.png) no-repeat center;
+        background-size: 20px 20px;
+        width: 36px;
+        height: 24px;
+        border-left: 1px solid var(--line-gridpanel-color);
+        top: 4px;
+        right: 0;
+      }
+    }
+  }
+  .toolbar-right {
+    .more-action {
+      width: 32px;
+      height: 32px;
+      background-image: url(@/assets/Icons/ic_More.png);
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: 24px 24px;
+      border-radius: 4px;
+    }
+    .btn-add {
+      margin-right: 8px;
+    }
+    .btn-export {
+      margin-right: 8px;
+    }
+  }
+}
+</style>
