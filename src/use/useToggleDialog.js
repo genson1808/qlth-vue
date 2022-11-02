b8ec2d736edbe08bad9506cd84d7b3e59ec14360
Sơn Trần <genson@Sons-MacBook-Pro.local>
2022-11-02 11:40:42 +0700

    feat: add some feature

---
+++ src/use/useToggleDialog.js
@@ -0,0 +1,13 @@
+import { inject } from "vue";
+
+/**
+ * composable thực hiệc cho việc muốn toggle dialog
+ * @param {*} stateName (string) tên của state app [{stateName}.dialog.show]
+ * @param {*} isClose [bool] flag muốn close hay open [true: close, false: open]
+ * @author SONTB
+ */
+export function useToggleDialog(stateName, isClose) {
+  const states = inject(stateName);
+  console.log(stateName);
+  isClose ? (states.dialog.show = false) : (states.dialog.show = true);
+}
